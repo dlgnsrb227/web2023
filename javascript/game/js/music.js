14009be91523f056d8e0d5511f0c028b5d8a2bdb
dlgnsrb227 <125418331+dlgnsrb227@users.noreply.github.com>
2023-04-25 18:12:52 +0900

    20230425

---
+++ javascript/game/js/music.js
@@ -0,0 +1,60 @@
+const allMusic = [
+    {
+        name : "1. 음악이름",
+        artist : "가수이름",
+        img : "music_view01",
+        audio : "music_audio01"
+    },{
+        name : "2. 음악이름",
+        artist : "가수이름",
+        img : "music_view02",
+        audio : "music_audio02"
+    },
+    {
+        name : "3. 음악이름",
+        artist : "가수이름",
+        img : "music_view03",
+        audio : "music_audio03"
+    },
+    {
+        name : "4. 음악이름",
+        artist : "가수이름",
+        img : "music_view04",
+        audio : "music_audio04"
+    },
+    {
+        name : "5. 음악이름",
+        artist : "가수이름",
+        img : "music_view05",
+        audio : "music_audio05"
+    },
+    {
+        name : "6. 음악이름",
+        artist : "가수이름",
+        img : "music_view06",
+        audio : "music_audio06"
+    },
+    {
+        name : "7. 음악이름",
+        artist : "가수이름",
+        img : "music_view07",
+        audio : "music_audio07"
+    },
+    {
+        name : "8. 음악이름",
+        artist : "가수이름",
+        img : "music_view08",
+        audio : "music_audio08"
+    },
+    {
+        name : "9. 음악이름",
+        artist : "가수이름",
+        img : "music_view09",
+        audio : "music_audio09"
+    },{
+        name : "10. 음악이름",
+        artist : "가수이름",
+        img : "music_view10",
+        audio : "music_audio10"
+    },
+]
