45ab6325f0f89237dc9889b63133c9ba25c832de
dlgnsrb227 <125418331+dlgnsrb227@users.noreply.github.com>
2023-04-26 17:34:47 +0900

    20230426

--- javascript/game/js/music.js
+++ javascript/game/js/music.js
@@ -57,4 +57,27 @@ const allMusic = [
         img : "music_view10",
         audio : "music_audio10"
     },
-]
+];
+
+const musicWrap = document.querySelector(".music__wrap");
+const musicName = musicWrap.querySelector(".music__control .title h3");
+const musicArtist = musicWrap.querySelector(".music__control .title p");
+const musicView = musicWrap.querySelector(".music__view .image img");
+const musicAudio = musicWrap.querySelector("#main-audio");
+
+let musicIndex = 1;     // 현재 음악 인덱스
+
+// 음악 재생
+const loadMusic = (num) => {
+    musicName.innerHTML = allMusic[num-1].name;         // 재생되고 있는 음악 제목 바꾸기
+    musicArtist.innerHTML = allMusic[num-1].artist;     // 재생되고 있는 음악 아티스트 바꾸기
+    musicView.src = `img/${allMusic[num-1].img}.png`;   // 재생되고 있는 음악 이미지 바꾸기
+    musicView.alt = allMusic[num-1].name;               // 재생되고 있는 음악 이미지(alt) 바꾸기
+    musicAudio.src = `audio/${allMusic[num-1].audio}.mp3`
+};
+
+window.addEventListener("load", () => {
+    loadMusic(musicIndex);
+
+    musicAudio.play();
+});
