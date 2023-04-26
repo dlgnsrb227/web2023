cf82aa88e0f49fd5c06507b0c55b9e16ab1d39ad
dlgnsrb227 <125418331+dlgnsrb227@users.noreply.github.com>
2023-04-26 18:01:13 +0900

    20230426

--- javascript/game/js/music.js
+++ javascript/game/js/music.js
@@ -64,17 +64,21 @@ const musicName = musicWrap.querySelector(".music__control .title h3");
 const musicArtist = musicWrap.querySelector(".music__control .title p");
 const musicView = musicWrap.querySelector(".music__view .image img");
 const musicAudio = musicWrap.querySelector("#main-audio");
+const musicPlay = musicWrap.querySelector("#control-play");
 
 let musicIndex = 1;     // 현재 음악 인덱스
 
 // 음악 재생
 const loadMusic = (num) => {
-    musicName.innerHTML = allMusic[num-1].name;         // 재생되고 있는 음악 제목 바꾸기
-    musicArtist.innerHTML = allMusic[num-1].artist;     // 재생되고 있는 음악 아티스트 바꾸기
-    musicView.src = `img/${allMusic[num-1].img}.png`;   // 재생되고 있는 음악 이미지 바꾸기
-    musicView.alt = allMusic[num-1].name;               // 재생되고 있는 음악 이미지(alt) 바꾸기
-    musicAudio.src = `audio/${allMusic[num-1].audio}.mp3`
+    musicName.innerHTML = allMusic[num-1].name;             // 재생되고 있는 음악 제목 바꾸기
+    musicArtist.innerHTML = allMusic[num-1].artist;         // 재생되고 있는 음악 아티스트 바꾸기
+    musicView.src = `img/${allMusic[num-1].img}.png`;       // 재생되고 있는 음악 이미지 바꾸기
+    musicView.alt = allMusic[num-1].name;                   // 재생되고 있는 음악 이미지(alt) 바꾸기
+    musicAudio.src = `audio/${allMusic[num-1].audio}.mp3`   // 재생되고 있는 음악 바꾸기
 };
+
+// 플레이버튼 클릭
+musicPlay.addEventListener("click", () => {});
 
 window.addEventListener("load", () => {
     loadMusic(musicIndex);
